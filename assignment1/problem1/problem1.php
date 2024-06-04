39b3b34486d846293d80abd9c53c65c1eff9afbc
Sina Ibn Amin <sinaibnamin@gmail.com>
2024-06-04 16:23:34 +0000

    ok

--- assignment1/problem1/problem1.php
+++ assignment1/problem1/problem1.php
@@ -2,11 +2,32 @@
 
 
 
-   $string = "-10 12 15 189 22 2 34";
+   function findmin($string)
+      {
+         $string = trim($string);
+         if(!$string){
+            return 'null';
+         }
 
-   $string_to_array = (explode(" ",$string));
+         $nums = (explode(" ",$string));
 
-   echo gettype(abs($string_to_array[0]));
+         if (count($nums) === 0) {
+            return 'null';
+         }
 
-    echo "\n";
+         $min = abs($nums[0]);
+         for ($i = 1; $i < count($nums); $i++) {
+            if (abs($nums[$i - 1]) > abs($nums[$i])) {
+                  $min = abs($nums[$i]);
+            }
+         }
+         return $min;
+      };
 
+
+echo findmin('5 6 1 3');
+echo "\n";
+echo findmin('55 6 2 3');
+echo "\n";
+echo findmin('5 6 -11 3');
+echo "\n";
--- assignment1/problem3/problem3.php
+++ assignment1/problem3/problem3.php
@@ -6,27 +6,58 @@ function reverse_word($sentence){
     if(empty($sentence)){
         return 'null';
     }
+ 
+    preg_match_all('/\w+|[^\w\s]+|\s+/', $sentence, $matches);
+    $words = $matches[0];
 
-    $sentence_to_array = explode(' ',$sentence);
+    $reverse_words_array = array();
 
-    $result = "";
-    
-    for($i = 0; $i < count($sentence_to_array); $i++){
-        $word = strrev ( $sentence_to_array[$i] );
-        if($i == count($sentence_to_array) - 1){
-            $result = $result.$word;
-        }else{
-            $result = $result.$word." ";
-        }
-        
+    foreach($words as $word){
+        $r_w = strrev($word);
+        array_push($reverse_words_array, $r_w);
     }
-    
-    return $result;
+
+    $final_string = implode("", $reverse_words_array);
+    return $final_string;
+
+   
 }
 
 echo reverse_word('I love programming');
+echo "\n";
+echo reverse_word('I love, programming.');
 echo "\n";
 echo reverse_word('I');
 echo "\n";
 echo reverse_word('');
 echo "\n";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
