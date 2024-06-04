af022e21be871ced0025d4e3acf0c8ab264b16d6
Sina Ibn Amin <sinaibnamin@gmail.com>
2024-06-04 15:05:47 +0000

    ok

--- assignment1/problem1/problem1.php
+++ assignment1/problem1/problem1.php
@@ -1,25 +1,12 @@
 <?php
-function findmin($nums)
-{
-    if (count($nums) === 0) {
-        return 'null';
-    }
-
-    $min = abs($nums[0]);
-    for ($i = 1; $i < count($nums); $i++) {
-        if (abs($nums[$i - 1]) > abs($nums[$i])) {
-            $min = abs($nums[$i]);
-        }
-    }
-    return $min;
-};
 
 
-echo findmin([3,4,5,6,2,2]);
-echo "\n";
-echo findmin([10,-12,34,12,-3,123]);
-echo "\n";
-echo findmin([-10]);
-echo "\n";
-echo findmin([]);
+
+   $string = "-10 12 15 189 22 2 34";
+
+   $string_to_array = (explode(" ",$string));
+
+   echo gettype(abs($string_to_array[0]));
+
+    echo "\n";
 
